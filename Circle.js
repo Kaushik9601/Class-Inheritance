f5417bf85e6468904c107452b5bca912583d5cd0
Kaushik9601 <93317518+Kaushik9601@users.noreply.github.com>
2021-11-17 15:42:53 +0530

    Add files via upload

---
+++ Circle.js
@@ -0,0 +1,11 @@
+const Shape =require("./shape")
+class Circle extends Shape{
+  super(radius){
+    this.radius= radius;
+  }
+
+     calculateArea(){
+    console.log('Area');
+     }
+  }
+  module.exports = Circle;
